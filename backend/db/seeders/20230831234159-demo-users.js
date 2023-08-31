a4df263032db217cd1986b1fda2fc0deddeaa48b
Travis <travisdanielbrown@proton.me>
2023-08-31 19:45:22 -0400

    added some seeder data

--- backend/db/seeders/20230831234159-demo-users.js
+++ backend/db/seeders/20230831234159-demo-users.js
@@ -12,16 +12,22 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     await User.bulkCreate([
       {
+        firstName: 'Tiger',
+        lastName: 'Woods',
         email: 'demo@user.io',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
+        firstName: 'Bob',
+        lastName: 'Barker',
         email: 'user1@user.io',
         username: 'FakeUser1',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
+        firstName: 'Matt',
+        lastName: 'Damon',
         email: 'user2@user.io',
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')
