a718b52ceee5add4533761759cf738d2c28a75a7
Travis <travisdanielbrown@proton.me>
2023-08-31 16:19:10 -0400

    created session log out

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,4 +1,11 @@
 const express = require('express')
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
+
+
 
 module.exports = router;
