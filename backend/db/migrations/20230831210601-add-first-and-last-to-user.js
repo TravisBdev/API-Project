faeecee522c036ed169d3b7a3cbefe2d21421ef2
Travis <travisdanielbrown@proton.me>
2023-08-31 19:03:49 -0400

    added schema identification to user migration

--- backend/db/migrations/20230831210601-add-first-and-last-to-user.js
+++ backend/db/migrations/20230831210601-add-first-and-last-to-user.js
@@ -1,20 +1,27 @@
 'use strict';
-
 /** @type {import('sequelize-cli').Migration} */
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+options.tableName = "Users";
+
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn('Users', 'firstName', {
+    await queryInterface.addColumn(options, 'firstName', {
       type: Sequelize.STRING,
       allowNull: false,
     });
-    await queryInterface.addColumn('Users', 'lastName', {
+    await queryInterface.addColumn(options, 'lastName', {
       type: Sequelize.STRING,
       allowNull: false,
     });
   },
 
+
+
   async down(queryInterface, Sequelize) {
-    await queryInterface.removeColumn('Users', 'firstName');
-    await queryInterface.removeColumn('Users', 'lastName');
+    await queryInterface.dropTable(options, 'firstName');
+    await queryInterface.dropTable(options, 'lastName');
   }
 };
