47e47f399a1ac8cc8505d278bbb3ac9afcb5673e
Travis <travisdanielbrown@proton.me>
2023-09-05 19:36:24 -0400

    created spotImages table and added seed data

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -13,6 +13,9 @@ module.exports = (sequelize, DataTypes) => {
       Spot.belongsTo(models.User, {
         foreignKey: 'ownerId'
       })
+      Spot.hasMany(models.SpotImage, {
+        foreignKey: 'spotId'
+      })
     }
   }
   Spot.init({
