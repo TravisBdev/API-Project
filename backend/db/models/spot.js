751b47ce77d11b242e43f5d15a2e7c4523127362
Travis <travisdanielbrown@proton.me>
2023-09-09 16:09:43 -0400

    fixed auth

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -34,7 +34,7 @@ module.exports = (sequelize, DataTypes) => {
   Spot.init({
     ownerId: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      allowNull: false,
     },
     address: {
       type: DataTypes.STRING,
