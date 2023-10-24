bd0ab2a4a17f688462ce964dfd87bb59b33e6c16
Travis <travisdanielbrown@proton.me>
2023-10-24 19:11:52 -0400

    finished nav-bar / profile-button / spot-tile

--- frontend/src/components/SpotTile/index.js
+++ frontend/src/components/SpotTile/index.js
@@ -6,17 +6,18 @@ const SpotTile = ({ spot }) => {
     <Link to={`/spots/${spot.id}`} className='link'>
       <div className="spot-tile">
         <div className="tooltip">{spot.name}</div>
-        <div className="img-container">
-          <img src={spot.previewImage} alt='' className="preview-img" />
+        <img src={spot.previewImage} alt='' className="preview-img" />
+        <div className="spot-details">
           <div className="location">
             {spot.city}, {spot.state}
           </div>
           <div className="avg-rating">
             {spot.avgRating ? <><i className="fa-solid fa-star fa-xs"></i> {spot.avgRating.toFixed(1)}</> : 'New'}
           </div>
-        </div>
-        <div className="price">
-          ${spot.price} night
+
+          <div className="price">
+            ${spot.price} night
+          </div>
         </div>
       </div>
     </Link>
